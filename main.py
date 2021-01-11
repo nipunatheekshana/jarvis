21f96254e0f346a7e42f60594660f423ad61725c
nipuna theekshana <nipunatheekshana8@gmail.com>
2021-01-12 00:52:03 +0530

    'timname'

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
 
 listner = sr.Recognizer()
 engine = pyttsx3.init()
@@ -31,9 +34,24 @@ def run_jarvis():
     command = receive_command()
     print(command)
     if 'play' in command:
-        song=command.replace('play','')
-        talk('playing'+song )
+        song = command.replace('play', '')
+        talk('playing'+song)
         pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I %M %p')  # I is 12 hour format of time
+        talk('current time is'+time)
+    elif 'who' in command:
+        query = command.replace('who', '')
+        result = wikipedia.summary(query, 1)
+        print(result)
+        talk(result)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'name' in command:
+        talk('i am jarvis your personal assistant')
+    else:
+        talk('please say again')
 
 
-run_jarvis()
+while True:
+    run_jarvis()
